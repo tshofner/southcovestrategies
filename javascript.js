b5f39b1ca44053a18daba5c4f7b1d20cd7af3c3d
Travis Shofner <traviseshofner@gmail.com>
2013-02-06 10:32:40 -0500

    menubar update

--- javascript.js
+++ javascript.js
@@ -1,49 +1,22 @@
-$(document).ready(function() {
-	// this will get the full URL at the address bar
-    var url = window.location.href; 
+$(document).ready(function(){
 
-    // passes on every "a" tag 
-    $("a").each(function() {
-            // checks if its the same on the address bar
-        if(url == (this.href)) { 
-            $(this).closest("div").replaceWith($('active')); //this doesn't work not sure why
-    
-        }
-    }); 
+	$("ul.subnav").parent().append("<span></span>"); //Only shows drop down trigger when js is enabled (Adds empty span tag after ul.subnav*)
 	
-	$('.home-menu').mouseover(function() {
-		$(this).css({'background-color': '#c5c5ad', 'border': 'solid #3a3a52', 'color': '#3a3a52'});
-		$('.home-menu a:link').css({'color': '#3a3a52'});
+	$("ul.topnav li span").click(function() { //When trigger is clicked...
+		
+		//Following events are applied to the subnav itself (moving subnav up and down)
+		$(this).parent().find("ul.subnav").slideDown('fast').show(); //Drop down the subnav on click
+
+		$(this).parent().hover(function() {
+		}, function(){	
+			$(this).parent().find("ul.subnav").slideUp('slow'); //When the mouse hovers out of the subnav, move it back up
+		});
+
+		//Following events are applied to the trigger (Hover events for the trigger)
+		}).hover(function() { 
+			$(this).addClass("subhover"); //On hover over, add class "subhover"
+		}, function(){	//On Hover Out
+			$(this).removeClass("subhover"); //On hover out, remove class "subhover"
 	});
-	$('.home-menu').mouseleave(function() {
-		$(this).css({'background-color': '#3a3a52', 'border': 'solid #c5c5ad', 'color': '#c5c5ad'});
-		$('.home-menu a:link').css({'color': '#c5c5ad'});
-	}); 
-	
-	$('.contact-menu').mouseover(function() {
-		$(this).css({'background-color': '#c5c5ad', 'border': 'solid #3a3a52', 'color': '#3a3a52'});
-		$('.contact-menu a:link').css({'color': '#3a3a52'});
-	});
-	$('.contact-menu').mouseleave(function() {
-		$(this).css({'background-color': '#3a3a52', 'border': 'solid #c5c5ad', 'color': '#c5c5ad'});
-		$('.contact-menu a:link').css({'color': '#c5c5ad'});
-	});
-	
-	$('.about-menu').mouseover(function() {
-		$(this).css({'background-color': '#c5c5ad', 'border': 'solid #3a3a52', 'color': '#3a3a52'});
-		$('.about-menu a:link').css({'color': '#3a3a52'});
-	});
-	$('.about-menu').mouseleave(function() {
-		$(this).css({'background-color': '#3a3a52', 'border': 'solid #c5c5ad', 'color': '#c5c5ad'});
-		$('.about-menu a:link').css({'color': '#c5c5ad'});
-	});
-	
-	$('.services-menu').mouseover(function() {
-		$(this).css({'background-color': '#c5c5ad', 'border': 'solid #3a3a52', 'color': '#3a3a52'});
-		$('.services-menu a:link').css({'color': '#3a3a52'});
-	});
-	$('.services-menu').mouseleave(function() {
-		$(this).css({'background-color': '#3a3a52', 'border': 'solid #c5c5ad', 'color': '#c5c5ad'});
-		$('.services-menu a:link').css({'color': '#c5c5ad'});
-	}); 
+
 });
